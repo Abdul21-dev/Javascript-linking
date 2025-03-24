b1af11dba0eab882f190699198b668f96a50a987
Abdul21-dev <abdulrahman21062004@gmail.com>
2025-03-24 22:52:14 +0530

    .

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //  let smallObjects = document.getElementsByClassName("oldImg");
 //  smallObjects[0].src = "https://tse1.mm.bing.net/th?id=OIP.Lo05I918j9hMgg1rkuQgnQHaEo&pid=Api&P=0&h=180";
 // the above codev is not working will find out why.
- document.addEventListener("DOMContentLoaded", function(){
-     let smallObjects = document.getElementsByClassName("oldImg");
-     for(let i=0; i<smallObjects.length; i++){
-         smallObjects[i].src = "https://tse1.mm.bing.net/th?id=OIP.Lo05I918j9hMgg1rkuQgnQHaEo&pid=Api&P=0&h=180";
-         console.log(` image number ${i+1} is changed`);
-     }
-});
+//  document.addEventListener("DOMContentLoaded", function(){
+//      let smallObjects = document.getElementsByClassName("oldImg");
+//      for(let i=0; i<smallObjects.length; i++){
+//          smallObjects[i].src = "https://tse1.mm.bing.net/th?id=OIP.Lo05I918j9hMgg1rkuQgnQHaEo&pid=Api&P=0&h=180";
+//          console.log(` image number ${i+1} is changed`);
+//      }
+// });
 // Query selector
 // document.querySelector("p");    // selects first p element
 // document.querySelector("#id1");  // selects first element with id=id1
