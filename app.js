cb91975fa7ad1f2bc28154a5af99206d6c39d2bf
Abdul21-dev <abdulrahman21062004@gmail.com>
2025-03-25 23:23:25 +0530

    .

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-//  let smallObjects = document.getElementsByClassName("oldImg");
-//  smallObjects[0].src = "https://tse1.mm.bing.net/th?id=OIP.Lo05I918j9hMgg1rkuQgnQHaEo&pid=Api&P=0&h=180";
-// the above codev is not working will find out why.
-//  document.addEventListener("DOMContentLoaded", function(){
-//      let smallObjects = document.getElementsByClassName("oldImg");
-//      for(let i=0; i<smallObjects.length; i++){
-//          smallObjects[i].src = "https://tse1.mm.bing.net/th?id=OIP.Lo05I918j9hMgg1rkuQgnQHaEo&pid=Api&P=0&h=180";
-//          console.log(` image number ${i+1} is changed`);
-//      }
-// });
-// Query selector
-// document.querySelector("p");    // selects first p element
-// document.querySelector("#id1");  // selects first element with id=id1
-//  document.addEventListener("DOMContentLoaded", function(){
-//  let obj1 = document.querySelector(".oldImg"); //selects first element with class = myclass
-// //  example of using any one
-//   obj1.src = "https://tse1.mm.bing.net/th?id=OIP.Lo05I918j9hMgg1rkuQgnQHaEo&pid=Api&P=0&h=180";
-//  });
+ let smallObjects = document.getElementsByClassName("oldImg");
+ smallObjects[0].src = "https://tse1.mm.bing.net/th?id=OIP.Lo05I918j9hMgg1rkuQgnQHaEo&pid=Api&P=0&h=180";
+the above codev is not working will find out why.
+ document.addEventListener("DOMContentLoaded", function(){
+     let smallObjects = document.getElementsByClassName("oldImg");
+     for(let i=0; i<smallObjects.length; i++){
+         smallObjects[i].src = "https://tse1.mm.bing.net/th?id=OIP.Lo05I918j9hMgg1rkuQgnQHaEo&pid=Api&P=0&h=180";
+         console.log(` image number ${i+1} is changed`);
+     }
+});
+Query selector
+document.querySelector("p");    // selects first p element
+document.querySelector("#id1");  // selects first element with id=id1
+ document.addEventListener("DOMContentLoaded", function(){
+ let obj1 = document.querySelector(".oldImg"); //selects first element with class = myclass
+//  example of using any one
+  obj1.src = "https://tse1.mm.bing.net/th?id=OIP.Lo05I918j9hMgg1rkuQgnQHaEo&pid=Api&P=0&h=180";
+ });
